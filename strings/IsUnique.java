8bda00a161df14c077c099ec0c7918dd57091439
Manuel Benavides <manuel@bybsoft.com>
2019-10-03 16:06:34 +0000

    is unique method

---
+++ strings/IsUnique.java
@@ -0,0 +1,59 @@
+import java.util.HashMap;
+
+public class IsUnique{
+
+    /**
+     * checks if all chars are unique in a string using a HashMap
+     * @param word
+     * @return boolean
+     */
+    static boolean uniqueCharsHashMap(String word){
+
+        int size = word.length();
+        if(size == 1){
+            return true;
+        }
+        HashMap <Character, Integer> charsHM = new HashMap <Character, Integer>();
+        Integer zero = 0;
+        for(int i = 0; i < size; i++){
+            Character letter = word.charAt(i);
+            
+            if(charsHM.containsKey(letter)){
+                return false;
+            }
+            charsHM.put(letter, zero);
+        }
+        return true;
+    }    
+
+    /**
+     * checks if all chars are unique using no extra data structure
+     * @param word
+     * @return boolean
+     */
+    static boolean uniqueChars(String word){
+
+        int size = word.length();
+        if(size == 1){
+            return true;
+        }
+        
+        for(int i = 0; i < size; i++){
+            char letter = word.charAt(i);
+        
+            for(int j = i+1; j < size; j++){
+                char letter2 = word.charAt(j);
+                if(letter == letter2){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }   
+
+    public static void main(String[] args) {
+        String word = "abfdertyujb";
+        System.out.println(uniqueCharsHashMap(word));
+        System.out.println(uniqueChars(word));
+    }
+}
